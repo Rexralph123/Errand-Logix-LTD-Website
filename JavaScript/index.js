ac05981f2ea89c9ee9ad08bca2a2701cbaaf9f97
Rexralph123 <michugonna917@gmail.com>
2026-02-04 21:01:11 +0100

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,20 @@
+//---------------INDEX.HTML------------------------//
+//------------HEADER SECTION-------------------//
+const signupBtn = document.querySelector('.signup-btn');
+const loginBtn = document.querySelector('.login-btn');
+
+signupBtn.addEventListener('click', () => {
+  alert('Coming Soon!');
+});
+
+loginBtn.addEventListener('click', () => {
+  alert('Coming Soon!');
+});
+
+
+//----------------TRACK SEARCH SECTION---------------------//
+const searchBtn = document.querySelector(".search-btn");
+
+  searchBtn.addEventListener("click", function () {
+    alert("Coming Soon!");
+  });
