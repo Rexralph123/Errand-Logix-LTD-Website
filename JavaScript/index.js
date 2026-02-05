e8449257051185b7be0436276ec567b805e16525
Rexralph123 <michugonna917@gmail.com>
2026-02-06 00:26:14 +0100

    Update index.js
    First update on javascript

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,14 @@
 //---------------INDEX.HTML------------------------//
+const btn = document.getElementById("menuBtn");
+const menu = document.getElementById("mobileMenu");
+
+btn.onclick = () => {
+  menu.style.display =
+    menu.style.display === "flex" ? "none" : "flex";
+};
+
+
+
 //------------HEADER SECTION-------------------//
 const signupBtn = document.querySelector('.signup-btn');
 const loginBtn = document.querySelector('.login-btn');
